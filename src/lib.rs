de946a749513781c9d51d3f68ac598a736b56500
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-03-31 15:59:06 +0900

    :package: add wasm packages

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
-  }
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+  #[wasm_bindgen(js_namespace=console)]
+  fn log(s: &str);
 }
 
+#[wasm_bindgen(start)]
+pub fn run() {
+  log("Hello, world!");
+}
