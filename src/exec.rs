e335f647d054d085849663336550d5713f2d45a9
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-04-04 02:14:43 +0900

    :tada: add new bugs yay

--- src/exec.rs
+++ src/exec.rs
@@ -1,5 +1,26 @@
+const TRY_MAX: i8 = std::i8::MAX;
+
 pub fn run (
   code: &Vec<char>
-) -> Vec::<i64> {
-  Vec::new()
+) -> Vec<i64> {
+  let mut pointer: (i32, i32) = (0, 0);
+  let mut direction: (i32, i32) = (1, 0);
+
+  let mut try_count = 0;
+  loop {
+    pointer.0 += direction.0;
+    pointer.1 += direction.1;
+    try_count += 1;
+
+    if try_count > TRY_MAX {
+      break;
+    }
+  }
+
+  if try_count > TRY_MAX {
+    vec![2i64]
+  } else {
+    vec![1i64]
+  }
 }
+
