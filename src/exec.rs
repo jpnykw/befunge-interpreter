6ba0cd71d4e15c401672e349b62cc8413ac90a5b
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-04-06 15:49:10 +0900

    :sparkles: 2-dimensional vector :construction: pattern matches

--- src/exec.rs
+++ src/exec.rs
@@ -1,26 +1,36 @@
-const TRY_MAX: i8 = std::i8::MAX;
+const TRY_MAX: i16 = std::i16::MAX;
 
 pub fn run (
-  code: &Vec<char>
+  input: &str
 ) -> Vec<i64> {
-  let mut pointer: (i32, i32) = (0, 0);
-  let mut direction: (i32, i32) = (1, 0);
-
+  let temp = input.split('\n').collect::<Vec<&str>>();
+  let mut code = temp.iter().map(|line| line.chars().collect::<Vec<char>>());
+  let mut direction: (usize, usize) = (1, 0); // (dx, dy)
+  let mut pointer: (usize, usize) = (0, 0); // (x, y)
   let mut try_count = 0;
+
   loop {
+    match try_count {
+      TRY_MAX => return vec![2i64], // over
+      _ => {
+        match code.nth(pointer.1) {
+          Some(line) => {
+            if line.len() <= pointer.0 { return vec![2i64]; }
+            match line[pointer.0] {
+              '@' => break,
+              _ => {}
+            };
+          },
+          None => return vec![2i64]
+        };
+      }
+    };
+
     pointer.0 += direction.0;
     pointer.1 += direction.1;
     try_count += 1;
-
-    if try_count >= TRY_MAX {
-      break;
-    }
   }
 
-  if try_count >= TRY_MAX {
-    vec![2i64]
-  } else {
-    vec![1i64]
-  }
+  vec![1i64] // finish
 }
 
