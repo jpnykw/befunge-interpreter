db12b41501e0b10f50da89aacac96e7555ee2cf5
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-04-08 17:47:43 +0900

    :construction: reject if code is too long

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,11 @@ pub fn run() {
 
 #[wasm_bindgen]
 pub fn read(input: &str){
+  if input.chars().count() as f32 > (std::i8::MAX as f32).powf(2f32) {
+    console::log("too long");
+    return;
+  }
+
   console::log("\n---");
   console::log(&format!("input -> {}", input));
 
